fd45bfb075d4866cc10e4d886dad823571b05eac
juanluis2021 <jsanchezpizarro@cifpfbmoll.eu>
2022-12-01 17:56:16 +0100

    examen entornos git

---
+++ ejercicio2.py
@@ -0,0 +1,11 @@
+print ("BIENVENIDO EJERCICIO N2")
+
+centigrados = int(input("dame la temperatura en centigrados:") ) 
+
+conversion = 9.0 / 5.0 * centigrados + 32 
+
+print ( "te doy la temperatura de conversion ")
+
+print('{} grados centigrados son {} grados Fahrenheit'.format(centigrados,conversion))
+
+
